4c87550056885711f7b9fb4d0ecc11e756ff7717
ajayprabhuvs <168707958+ajayprabhuvs@users.noreply.github.com>
2024-09-25 11:12:49 +0530

    Create test.php

---
+++ test/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: test
+ * Description: Payment plugin
+ * Version: 1.0.0
+ * Author: Tryzens
+ * Author URI: test.com
+ *
+ * @package fp
+ */
+
+
+
+echo 'heeellloooooooo';
